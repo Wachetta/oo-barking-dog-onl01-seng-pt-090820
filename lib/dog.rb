ec1b80876bdd11614f2593d1b6b437bff3d25301
Zachary Darneille <zakdarn@gmail.com>
2020-09-23 18:58:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,15 @@
 # Your code goes here!
 class Dog
-  fido = Dog.new 
-  
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
   def name
-    @this_dogs_name = name 
+    @this_dogs_name
+  end
+end
+ 
+fido = Dog.new
+fido.name = "Fido"
+ 
+puts fido.name
